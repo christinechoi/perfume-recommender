3022730a16129af7ec5f6a139c9f194fd507e759
christinechoi <smi77y@gmail.com>
2017-11-24 22:04:43 -0500

    Create User model and add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true,
+                  uniqueness: { case_sensitive: false },
+                  format: /@/
+
 end
