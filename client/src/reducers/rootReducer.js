1f7e09bdb9f0d4848b245b4bb8bc59f08c8ee27b
christinechoi <smi77y@gmail.com>
2017-11-28 14:12:11 -0500

    Add user reducer to root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import perfumesReducer from './perfumesReducer';
+import userReducer from './userReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  perfumes: perfumesReducer
+  perfumes: perfumesReducer,
+  user: userReducer
 });
 
 export default rootReducer;
