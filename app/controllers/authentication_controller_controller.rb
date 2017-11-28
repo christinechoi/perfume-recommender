8b8a7cbdc770a720f4968d75d0ed1d8d4a030914
christinechoi <smi77y@gmail.com>
2017-11-28 14:07:01 -0500

    Add index method and fix typos

--- app/controllers/authentication_controller_controller.rb
+++ app/controllers/authentication_controller_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationControllerController < ApplicationController
    user = User.find_by(email: params[:email])
   if user && user.authenticate(params[:password])
     auth_token = JsonWebToken.encode({user_id: user.id})
-    render son: {auth_token: auth_token}, status: :ok
+    render json: {auth_token: auth_token}, status: :ok
   else
     render json: {error: 'Login Unsuccessfull'}, status: :unauthorized
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
+require 'JsonWebToken'
+
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users.to_json
+
+  end
 
 
   def create
+    # binding.pry
     user = User.new(user_params)
     if user.save
-      render json: { message: 'User created successfully' },
-             status: :created
+      render json: { message: 'User created successfully', user: user.to_json },
+             status: 201
     else
       render json: { errors: user.errors.full_messages },
              status: :bad_request
@@ -15,10 +24,10 @@ class UsersController < ApplicationController
 
   def login
     user = User.find_by(email: params[:email].to_s.downcase)
-
+    # binding.pry
     if user && user.authenticate(params[:password])
-        auth_token = JsonWebToken.encode({user_id: user.id})
-        render json: {auth_token: auth_token}, status: :ok
+        id_token = JsonWebToken.encode({user_id: user.id})
+        render json: {token: id_token, status: :ok}
     else
       render json: {error: 'Invalid username / password'}, status: :unauthorized
     end
