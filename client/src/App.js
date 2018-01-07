0267734923e7b9c68ba664ab448622d53addde95
christinechoi <smi77y@gmail.com>
2018-01-06 22:51:58 -0500

    Import semantic ui components individually

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,10 @@ import RecommendationsContainer from './containers/RecommendationsContainer';
 import SavedContainer from './containers/SavedContainer';
 import SignInContainer from './containers/SignInContainer';
 import LogInContainer from './containers/LogInContainer';
-import { Button, Container, Segment, Menu } from './semantic-ui/dist/components';
+import { Button } from './semantic-ui/dist/components/button.min.css';
+import { Container } from './semantic-ui/dist/components/container.css';
+import { Segment } from './semantic-ui/dist/components/segment.min.css';
+import { Menu } from './semantic-ui/dist/components/.min.css';
 
 class App extends Component {
   render() {
