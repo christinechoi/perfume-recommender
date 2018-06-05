c19834d29ae5cc8f2bdaddfc0af3ea1a81a5df57
christinechoi <smi77y@gmail.com>
2018-06-05 00:53:36 -0400

    Clean up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,23 +13,14 @@ class UsersController < ApplicationController
     # binding.pry
     user = User.new(user_params)
     if user.save
+      binding.pry
+      authenticate_for(user)
+
       render json: { message: 'User created successfully', user: user.to_json },
              status: 201
     else
       render json: { errors: user.errors.full_messages },
              status: :bad_request
-    end
-  end
-
-
-  def login
-    user = User.find_by(email: params[:email].to_s.downcase)
-    # binding.pry
-    if user && user.authenticate(params[:password])
-        id_token = JsonWebToken.encode({user_id: user.id})
-        render json: {token: id_token, status: :ok}
-    else
-      render json: {error: 'Invalid username / password'}, status: :unauthorized
     end
   end
 
